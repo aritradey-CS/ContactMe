8ede6bafad39c41b64201efc8450ad47665e2201
aritradey-CS <81703791+aritradey-CS@users.noreply.github.com>
2023-05-14 21:00:43 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import AddContact from './AddContacts';
+import ContactCard from './ContactCard';
+import ContactList from './ContactList';
+
+function App() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Id beatae iste consequatur nemo cupiditate? Culpa ex ad itaque, totam ullam recusandae iste cum numquam! Maiores, itaque magni nemo voluptate labore vel quibusdam nostrum! A corrupti aperiam expedita nostrum sed officia pariatur perferendis quibusdam minima tenetur.
+    </div>
+  )
+}
+
+export default App;
